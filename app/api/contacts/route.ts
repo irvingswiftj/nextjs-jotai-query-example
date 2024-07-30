5c374917148e9fe5ec531604eeebaf4b7a4f14b5
James Irving-Swift <james@irving-swift.com>
2024-07-30 09:40:47 +0200

    Simplified the api so that contacts is a local variable and no need for mongodb

--- app/api/contacts/route.ts
+++ app/api/contacts/route.ts
@@ -1,39 +1,15 @@
 import { NextResponse } from "next/server";
-import { findOrCreateUser, updateUser } from "@/models/Users";
 
-const MOCK_USER = {
-  email: "foo@bar.com",
-  name: "mr foo bar",
-};
+let contactList: [] = [];
 
 export const GET = async function contacts(req) {
-  const user = MOCK_USER;
-
-  if (!user || !user.email) {
-    return NextResponse.json(
-      { success: false, message: "User not found" },
-      { status: 404 }
-    );
-  }
-
-  const { contactList } = await findOrCreateUser(user);
-
   return NextResponse.json(contactList);
 };
 
 export const POST = async function contacts(req) {
-  const user = MOCK_USER;
-
-  if (!user || !user.email) {
-    return NextResponse.json(
-      { success: false, message: "User not found" },
-      { status: 404 }
-    );
-  }
-
   const body = await req.json();
 
-  const result = await updateUser(user, body);
+  contactList = body;
 
-  return NextResponse.json(result.contactList);
+  return NextResponse.json(contactList);
 };
